7a1aeb49f4acef46b5d833cd0cf33a4942ed0a9a
twkesq <skuzemchenko50@gmail.com>
2023-03-31 23:47:20 +0300

    final commit , v.4.1

--- src/components/heroimg1/HeroImg.js
+++ src/components/heroimg1/HeroImg.js
@@ -4,13 +4,15 @@ import IntroImg from "../../assets/intro-bg.jpg"
 import { Link } from "react-router-dom";
 import "../../index.css";
 import 'animate.css';
+import Typed from 'react-typed';
+
 const HeroImg = () => {
   return <div className="hero">
     <div className="mask">
     </div>
     <div className="content">
         <p className="animate__animated animate__fadeInDown  animate__delay-1s animate__slow">HI! I'm Freelancer.</p>
-        <h1 className="animate__animated animate__fadeInDown  animate__delay-1s animate__slow">Frontend Developer</h1>
+        <h1><Typed strings={["Frontend Developer"]} typeSpeed={120}/></h1>
         <div>
           <Link to="/project" className="btn">Projects</Link>
           <Link to="/contact" className="btn btn-light">Contact</Link>
